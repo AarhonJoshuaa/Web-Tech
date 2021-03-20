b296ede567e82762f779a01cf1637fc893c87c36
AarhonJoshuaa <aarhonjoshuaa001@gmail.com>
2021-03-20 18:38:27 +0530

    added urls

--- charity/main/urls.py
+++ charity/main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index"),path("about.html", views.about, name="about")]
+urlpatterns = [path("", views.index, name="index"),path("about.html", views.about, name="about"),path("blog.html", views.blog, name="blog"),path("contact.html", views.contact, name="contact")]
 
 
  
--- charity/main/views.py
+++ charity/main/views.py
@@ -5,3 +5,7 @@ def index(request):
     return render(request,"index.html")
 def about(request):
     return render(request,"about.html")
+def contact(request):
+    return render(request,"contact.html")
+def blog(request):
+    return render(request,"blog.html")
